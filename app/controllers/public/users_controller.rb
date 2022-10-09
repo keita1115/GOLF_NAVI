c164e4332140d929b7a96814f5f1bb1522e2a307
keita1115 <keita.hashimoto1115@gmail.com>
2022-10-09 03:58:33 +0000

    マイページ制作

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -4,13 +4,22 @@ def index
 end
 
 def show
-
+   @user = User.find(params[:id])
 end
 
 def edit
+   @user = current_user
 end
 
 def update
+  @user = current_user
+  @user.update(user_params)
+   redirect_to user_path(current_user)
+end
+
+
+def user_params
+  params.require(:user).permit(:email, :name)
 end
 
 end
