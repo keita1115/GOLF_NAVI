5c478463128278ba55c4efcac0939b697f138330
keita1115 <keita.hashimoto1115@gmail.com>
2022-10-11 06:26:32 +0000

    コメント機能　 非同期

--- app/controllers/public/comments_controller.rb
+++ app/controllers/public/comments_controller.rb
@@ -4,19 +4,15 @@ class Public::CommentsController < ApplicationController
     @course = Course.find(params[:course_id])
     @comment = current_user.comments.new(comment_params)
     @comment.course_id = @course.id
-    if @comment.save
-      flash.now[:notice] = 'コメントを投稿しました'
-      render :course_comments
-    else
-      render :error  #render先にjsファイルを指定
-    end
+    @comment.save
+    #render :comment
   end
 
   def destroy
-    Comment.find_by(id: params[:id], course_id: params[:course_id]).destroy
-    flash.now[:alert] = '投稿を削除しました'
+    course = Comment.find(params[:id])
+    course.destroy
     @course = Course.find(params[:course_id])
-     render :course_comments
+    #render :comment
   end
 
   private
