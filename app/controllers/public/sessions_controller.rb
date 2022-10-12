41436d281e3566edf227ff3f70972caa1c22c1e8
keita1115 <keita.hashimoto1115@gmail.com>
2022-10-12 14:11:55 +0000

    TOPページ　作成

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -25,13 +25,13 @@ class Public::SessionsController < Devise::SessionsController
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
   def after_sign_in_path_for(resource)
-    users_path
+    root_path
   end
 
   def after_sign_out_path_for(resource)
     root_path
   end
-  
+
   def guest_sign_in
     user = User.guest
     sign_in user
