69a13272ced24b962c6bc3ec1cb6cc0abbf5ff7d
keita1115 <keita.hashimoto1115@gmail.com>
2022-10-14 12:34:41 +0000

    admin コメント機能追加

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -8,6 +8,7 @@ end
 def show
   redirect_to root_path, notice: 'ゲストユーザーはこの操作は出来ません。' if current_user.name == 'guestuser'
   @user = User.find(params[:id])
+  @courses = Course.all.page(params[:page]).per(3)
 end
 
 def edit
