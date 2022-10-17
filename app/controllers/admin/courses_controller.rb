956bf451af04a28b0be6089ac6a87cae11287b55
keita1115 <keita.hashimoto1115@gmail.com>
2022-10-17 11:46:38 +0000

    管理者側　投稿削除

--- app/controllers/admin/courses_controller.rb
+++ app/controllers/admin/courses_controller.rb
@@ -1,2 +1,20 @@
 class Admin::CoursesController < ApplicationController
 
+
+def show
+   @course = Course.find(params[:id])
+end
+
+def destroy
+   course = Course.find(params[:id])
+   course.destroy
+   redirect_to admin_root_path
+end
+
+private
+
+def course_params
+   params.require(:course).permit(:title, :body, :image, :user_id, :genre_id, :score, :address)
+end
+
+end
--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -1,6 +1,7 @@
 class Admin::HomesController < ApplicationController
 
 def top
+   @courses = Course.all.page(params[:page]).per(10)
 end
 
 end
