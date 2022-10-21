edfd63eaf10c4f85559f547a8e03da4c0ae1a1b2
keita1115 <keita.hashimoto1115@gmail.com>
2022-10-21 10:24:38 +0000

    course.rb 編集

--- app/models/course.rb
+++ app/models/course.rb
@@ -5,9 +5,6 @@ class Course < ApplicationRecord
   belongs_to :genre, optional: true
   has_many :favorites ,dependent: :destroy
   has_many :comments, dependent: :destroy
-  # def get_course_image
-  #  (image.attached?) ? image : "no_image.jpg"
-  # end
 
   def favorites?(user)
     favorites.where(user_id: user.id).exists?
