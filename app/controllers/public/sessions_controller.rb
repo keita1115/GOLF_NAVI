f5a9884317389664907f26ead4b10259bdc4eee1
keita1115 <keita.hashimoto1115@gmail.com>
2022-10-23 13:21:28 +0000

    ゲストユーザー 編集

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -36,7 +36,7 @@ class Public::SessionsController < Devise::SessionsController
   def guest_sign_in
     user = User.guest
     sign_in user
-    redirect_to user_path(user), notice: 'guestuserでログインしました。'
+    redirect_to root_path(user), notice: 'guestuserでログインしました。'
   end
 
   protected
